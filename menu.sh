80d9ac97f227fb606dab8845e0cde1133de5dd20
xubiaolin <xubiaolin@markxu.vip>
2025-03-05 21:10:28 +0800

    pushgit

--- menu.sh
+++ menu.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
 
 
-function menu() {
-    echo "1. 安装 Headscale"
-    echo "2. 安装 Derper"
-    echo "3. 安装 Derp-File"
-    echo "4. 安装 Headscale"
+function gen_cert() {
+    source .env
+    if [ -z "$DERP_DOMAIN" ]; then
+        echo "错误: DERP_DOMAIN 环境变量未设置"
+        exit 1
+    fi
+    
+    mkdir -p certs
+    cd certs
+    
+    openssl req -x509 -newkey rsa:4096 -sha256 -days 36500 -nodes -keyout ${DERP_DOMAIN}.key -out ${DERP_DOMAIN}.crt -subj "/CN=${DERP_DOMAIN}" -addext "subjectAltName=IP:${DERP_DOMAIN}"
+    cd ..
+    echo "为 ${DERP_DOMAIN} 生成证书成功"
 }
+
+function menu() {
+    echo "1. 生成证书"
+    echo "0. 退出"
+    read -p "请输入选项: " choice
+    case $choice in
+        1)
+            gen_cert
+        ;;
+        0)
+            exit 0
+        ;;
+        *)
+            echo "无效选项"
+        ;;
+    esac
+}
+
+menu
